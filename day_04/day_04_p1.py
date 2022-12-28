04e9a002083249ed87d8edd7416b1c0e68f7d4fc
Rick Ortega <ricko@odusa.org>
2022-12-28 09:03:41 -0800

    refactor day4 p1

--- day_04/day_04_p1.py
+++ day_04/day_04_p1.py
@@ -38,21 +38,25 @@ class Board():
         self.left: set[int] = left
         self.ints: list[int] = ints
         self.winner: bool = winner
+    def __str__(self):
+        return (
+            f'Board object\nNumbers left: {self.left}\n'
+            f'Initial Board numbers: {self.ints}')
 
     @property
     def has_won(self):
         return self.winner
 
     def discard_number(self, number: int):
-        print(f'discarding number: {number}')
         self.left.discard(number)
         self.check_rows()
         self.check_cols()
         if self.winner:
-            print('\t\t - WINNER - ')
-
+            print(f'\t\t - WINNER - \n\t\tWinning number = {number}')
+            print(f'sum of left = {sum(self.left)}')
+            print(f'sum(self.left) * number = {sum(self.left) * number}')
+    
     def check_rows(self) -> bool:
-        #print('checking rows...')
         for i in range(5):
             num_found = 0
             for j in range(5):
@@ -60,11 +64,9 @@ class Board():
                 if number in self.left:
                     num_found += 1
             if num_found == 0:
-                #print(f'winner found in row {i+1}')
                 self.winner = True
          
     def check_cols(self) -> bool:
-        #print('checking cols...')
         for i in range(5):
             num_found = 0
             for j in range(5):
@@ -72,7 +74,6 @@ class Board():
                 if number_to_check in self.left:
                     num_found += 1
             if num_found == 0:
-                #print(f'winner found in col {i+1}')
                 self.winner = True
 
     @classmethod
@@ -84,31 +85,42 @@ class Board():
 boards = [Board.parse(board) for board in input]
 ints = [int(s) for s in numbers.split(',')]
 
-# def get_number() -> int:
-#     for number in ints:
-#         for board in boards:
-#             board.discard_number(number)
-#             if board.winner:
-#                 return sum(board.left) * number
-#     raise AssertionError('unreachable')
-
-
-
-def return_last_winner_number() -> int:
-    winner_list = []
+def get_first_winning_board(ints: list(str), boards: list(Board)) -> Board:
     for number in ints:
         for board in boards:
             board.discard_number(number)
             if board.winner:
-                winner_list.append(board)
+                return (board, number)
+    raise AssertionError('There really should be a winner')
 
-    return winner_list[-1]
+def get_winning_number(board: Board, number: int) -> int:
+    return sum(board.left) * number
 
-def calculate_score(board: Board):
-    for number in ints:
-        board.discard_number(number)
-        if board.winner:
-            return sum(board.left) * number
+winner_board, winning_number = get_first_winning_board(ints, boards)
+print(str(winner_board))
+winning_number = get_winning_number(winner_board, winning_number)
+print(f'winning number is {winning_number}')
 
-last_winner = return_last_winner_number()
+
+# def get_winner_list(ints: list(str), boards: list(Board)) -> list(Board):
+#     winner_list = []    
+#     for board in boards:
+#         for num in ints:
+#             board.discard_number(num)
+#             if board.winner:
+#                 winner_list.append(board)
+#                 break
+#     return winner_list
+
+# def get_last_winner(boards: list(Board)) -> Board:
+#     return boards[-1]
+
+# def calculate_score(board: Board):
+#     for number in ints:
+#         board.discard_number(number)
+#         if board.winner:
+#             return sum(board.left) * number
+
+# winner_list = get_winner_list(ints, boards)
+# last_winner = get_last_winner(winner_list)
 # print(calculate_score(last_winner))
